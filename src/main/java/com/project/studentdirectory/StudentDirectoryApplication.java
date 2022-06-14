76f46ff3818ecd0fb39df6c2789adada8ab743a2
gsmith <gsmith340213@gmail.com>
2022-06-14 08:29:23 -0500

    Initial commit

---
+++ src/main/java/com/project/studentdirectory/StudentDirectoryApplication.java
@@ -0,0 +1,12 @@
+package com.project.studentdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentDirectoryApplication.class, args);
+    }
+}
